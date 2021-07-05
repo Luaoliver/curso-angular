afb8ae225d902360356d862f11d186bf2db2ecc3
luaoliver <lulu-so@hotmail.com>
2021-07-05 10:19:21 -0300

    reviewing typescript for angular

---
+++ hello-typescript/main.ts
@@ -0,0 +1,25 @@
+var minhaVar = 'minha variável';
+
+function minhaFunc(x, y) {
+  return x + y;
+}
+
+let num = 2;
+
+const PI = 3.4;
+
+var numeros = [1, 2, 3];
+numeros.map(function(valor) {
+  return valor * 2;
+});
+
+numeros.map(valor => valor * 2)
+
+class Matematica {
+  soma (x, y){
+    return x + y;
+  }
+}
+
+var n1: any = 'sjdhj';
+n1 = 4;
