9028d55825aa6af24ee1e92a038c347116a16533
luaoliver <lulu-so@hotmail.com>
2021-07-07 10:13:14 -0300

    using an event emitter

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -44,6 +44,10 @@ export class DataBindingComponent implements OnInit {
     this.isMouseOver = !this.isMouseOver;
   }
 
+  onMudouValor(evento: any) {
+    console.log(evento);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
