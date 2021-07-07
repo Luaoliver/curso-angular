9ac3b07ca5efad6cdad6685eb2c894337772fd08
luaoliver <lulu-so@hotmail.com>
2021-07-07 09:31:01 -0300

    declaring input as metadata (unusual)

--- data-binding/src/app/input-property/input-property.component.ts
+++ data-binding/src/app/input-property/input-property.component.ts
@@ -3,11 +3,12 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-curso',
   templateUrl: './input-property.component.html',
-  styleUrls: ['./input-property.component.css']
+  styleUrls: ['./input-property.component.css'],
+  inputs: ['nomeCurso:nome']
 })
 export class InputPropertyComponent implements OnInit {
 
-  @Input('nome') nomeCurso: string = '';
+  nomeCurso: string = '';
 
   constructor() { }
 
