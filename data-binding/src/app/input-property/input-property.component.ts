831055c6438ead82743da2d6fd1fc045b0d9ae50
luaoliver <lulu-so@hotmail.com>
2021-07-07 09:35:10 -0300

    finished input property

--- data-binding/src/app/input-property/input-property.component.ts
+++ data-binding/src/app/input-property/input-property.component.ts
@@ -3,12 +3,11 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-curso',
   templateUrl: './input-property.component.html',
-  styleUrls: ['./input-property.component.css'],
-  inputs: ['nomeCurso:nome']
+  styleUrls: ['./input-property.component.css']
 })
 export class InputPropertyComponent implements OnInit {
 
-  nomeCurso: string = '';
+  @Input('nome') nomeCurso: string = '';
 
   constructor() { }
 
